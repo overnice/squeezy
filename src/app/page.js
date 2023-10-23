1c23de56bb3297d35463cf7c911031b3672bcae4
Julian Bauer <julian@overnice.com>
2023-10-23 22:38:06 +0200

    grid section

--- src/app/page.js
+++ src/app/page.js
@@ -9,6 +9,7 @@ export default function Home() {
   const [snappedWidth, setSnappedWidth] = useState();
   const [renderedLetters, setRenderedLetters] = useState([currentLetter]);
   const [renderedLettersWidths, setRenderedLettersWidths] = useState([]);
+  const [showcasedLetter, setShowcasedLetter] = useState("A");
 
   const themes = ["red", "blue", "pink", "black"];
   const [theme, setTheme] = useState(0);
@@ -26,7 +27,7 @@ export default function Home() {
   // const gridLines = [400, 420, 440, 460, 480, 500, 520, 540, 560, 580, 600, 620, 640, 660, 680, 700];
   const gridLines = [400, 430, 460, 490, 520, 550, 580, 610, 640, 670, 700];
 
-  const letterSet = [
+  const heroLetterSet = [
     "A",
     "B",
     "C",
@@ -46,17 +47,141 @@ export default function Home() {
     "Z",
   ];
 
-  const gridsRef = useRef(null);
-  const gridRef = useRef(null);
+  const fullLetterSet = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+
+  const regularLetters = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "J",
+    "K",
+    "L",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "U",
+    "V",
+    "X",
+    "Y",
+    "Z",
+  ];
+  const wideLetters = ["M", "T", "W"];
+  const narrowLetters = ["I"];
+  const centerSlitLetters = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "J",
+    "K",
+    "L",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "U",
+    "V",
+    "X",
+    "Y",
+    "Z",
+  ];
+  const centerWideGapLetters = [
+    "A",
+    "B",
+    "H",
+    "K",
+    "R",
+    "N",
+    "X",
+    "S",
+    "Y",
+    "Z",
+  ];
+  const centerGapLetters = ["B", "C", "X"];
+  const centerWideBottomEdgeLetters = ["P"];
+  const centerBottomEdgeLetters = ["J", "L", "K"];
+  const centerTopEdgeLetters = ["R"];
+  const doubleSlitLetters = ["M", "N", "T", "W"];
+  const topGapLetters = ["G"];
+  const topEnclosedLetters = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "M",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "Z",
+  ];
+  const bottomEnclosedLetters = [
+    "B",
+    "C",
+    "D",
+    "E",
+    "G",
+    "J",
+    "L",
+    "O",
+    "Q",
+    "S",
+    "U",
+    "V",
+    "W",
+    "Y",
+    "Z",
+  ];
+
   const currentLetterRef = useRef(null);
-  const editableRef = useRef(null);
-  const letter1Ref = useRef(null);
-  const letter2Ref = useRef(null);
-  const letter3Ref = useRef(null);
 
   const min = 0.6080125;
   const max = 1.0880125;
-  const increment = 0.024;
 
   useEffect(() => {
     let x = 0;
@@ -86,31 +211,6 @@ export default function Home() {
       if (delta >= 0 && delta <= (getWidth(max) - getWidth(min)) / 2) {
         setCurrentWidth(width);
       }
-
-      currentLetterRef.current.style.setProperty(
-        `font-variation-settings`,
-        `"wdth" ${currentWidth}`
-      );
-
-      letter1Ref.current.style.setProperty(
-        `font-variation-settings`,
-        `"wdth" ${renderedLettersWidths[renderedLettersWidths.length - 1]}`
-      );
-
-      letter2Ref.current.style.setProperty(
-        `font-variation-settings`,
-        `"wdth" ${renderedLettersWidths[renderedLettersWidths.length - 2]}`
-      );
-
-      letter3Ref.current.style.setProperty(
-        `font-variation-settings`,
-        `"wdth" ${renderedLettersWidths[renderedLettersWidths.length - 3]}`
-      );
-
-      editableRef.current.style.setProperty(
-        `font-variation-settings`,
-        `"wdth" ${currentWidth}`
-      );
     };
 
     const handleResize = () => {
@@ -173,10 +273,10 @@ export default function Home() {
 
   useEffect(() => {
     const random = () => {
-      return Math.floor(Math.random() * letterSet.length);
+      return Math.floor(Math.random() * heroLetterSet.length);
     };
 
-    setCurrentLetter(letterSet[random()]);
+    setCurrentLetter(heroLetterSet[random()]);
     setRenderedLetters((v) => [...v, currentLetter]);
     setRenderedLettersWidths((v) => [...v, snappedWidth]);
 
@@ -203,23 +303,41 @@ export default function Home() {
         <div className={styles.letters}>
           <div
             className={`${styles.letter} ${styles.letter3}`}
-            ref={letter3Ref}
+            style={{
+              fontVariationSettings: `"wdth" ${
+                renderedLettersWidths[renderedLettersWidths.length - 3]
+              }`,
+            }}
           >
             {renderedLetters[renderedLetters.length - 3]}
           </div>
           <div
             className={`${styles.letter} ${styles.letter2}`}
-            ref={letter2Ref}
+            style={{
+              fontVariationSettings: `"wdth" ${
+                renderedLettersWidths[renderedLettersWidths.length - 2]
+              }`,
+            }}
           >
             {renderedLetters[renderedLetters.length - 2]}
           </div>
           <div
             className={`${styles.letter} ${styles.letter1}`}
-            ref={letter1Ref}
+            style={{
+              fontVariationSettings: `"wdth" ${
+                renderedLettersWidths[renderedLettersWidths.length - 1]
+              }`,
+            }}
           >
             {renderedLetters[renderedLetters.length - 1]}
           </div>
-          <div className={styles.letter} ref={currentLetterRef}>
+          <div
+            className={styles.letter}
+            ref={currentLetterRef}
+            style={{
+              fontVariationSettings: `"wdth" ${currentWidth}`,
+            }}
+          >
             {currentLetter}
           </div>
         </div>
@@ -227,12 +345,101 @@ export default function Home() {
       <section className={styles.editor}>
         <p
           className={styles.editableArea}
-          ref={editableRef}
           contentEditable
           suppressContentEditableWarning
+          style={{
+            fontVariationSettings: `"wdth" ${currentWidth}`,
+          }}
         >
           Type anything
         </p>
+      </section>
+      <section className={styles.gridContainer}>
+        <div className={styles.grid}>
+          {fullLetterSet.map((letter, key) => {
+            return (
+              <div
+                className={styles.gridItem}
+                key={key}
+                style={
+                  {
+                    // fontVariationSettings: `"wdth" ${currentWidth}`,
+                  }
+                }
+                onMouseMove={() => setShowcasedLetter(letter)}
+              >
+                {letter}
+              </div>
+            );
+          })}
+        </div>
+        <div
+          className={styles.gridShowcase}
+          style={
+            {
+              // fontVariationSettings: `"wdth" ${currentWidth}`,
+            }
+          }
+        >
+          {showcasedLetter}
+          <div
+            className={`${styles.gridLines} ${
+              regularLetters.includes(showcasedLetter) ? styles.regular : ""
+            } 
+            ${wideLetters.includes(showcasedLetter) ? styles.wide : ""} ${
+              narrowLetters.includes(showcasedLetter) ? styles.narrow : ""
+            } ${
+              centerSlitLetters.includes(showcasedLetter)
+                ? styles.centerSlit
+                : ""
+            } ${
+              doubleSlitLetters.includes(showcasedLetter)
+                ? styles.doubleSlit
+                : ""
+            } ${
+              centerWideGapLetters.includes(showcasedLetter)
+                ? styles.centerWideGap
+                : ""
+            } ${
+              centerGapLetters.includes(showcasedLetter) ? styles.centerGap : ""
+            } ${
+              centerWideBottomEdgeLetters.includes(showcasedLetter)
+                ? styles.centerWideBottomEdge
+                : ""
+            } ${
+              centerTopEdgeLetters.includes(showcasedLetter)
+                ? styles.centerTopEdge
+                : ""
+            } ${
+              centerBottomEdgeLetters.includes(showcasedLetter)
+                ? styles.centerBottomEdge
+                : ""
+            } ${topGapLetters.includes(showcasedLetter) ? styles.topGap : ""} ${
+              topEnclosedLetters.includes(showcasedLetter)
+                ? styles.topEnclosed
+                : ""
+            } ${
+              bottomEnclosedLetters.includes(showcasedLetter)
+                ? styles.bottomEnclosed
+                : ""
+            }`}
+          >
+            <div className={styles.left}></div>
+            <div className={styles.right}></div>
+            <div className={styles.top}></div>
+            <div className={styles.bottom}></div>
+            <div className={styles.centerTop}></div>
+            <div className={styles.centerBottom}></div>
+            <div className={styles.centerWideTop}></div>
+            <div className={styles.centerWideBottom}></div>
+            <div className={styles.centerLeft}></div>
+            <div className={styles.centerRight}></div>
+            <div className={styles.centerTwoLeft}></div>
+            <div className={styles.centerTwoRight}></div>
+            <div className={styles.outerTop}></div>
+            <div className={styles.outerBottom}></div>
+          </div>
+        </div>
       </section>
     </main>
   );
