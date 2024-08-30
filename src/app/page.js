5be370f6490a46797db209d885520feb444f7388
Jon Snow <jonny96snow@googlemail.com>
2024-08-30 11:55:02 +0100

    Add anime.js animation

--- src/app/page.js
+++ src/app/page.js
@@ -3,7 +3,7 @@
 import React, { useEffect, useRef, useState } from "react";
 import Head from "next/head";
 import styles from "./page.module.css";
-
+import anime from "animejs";
 import localFont from "next/font/local";
 
 // Font files can be colocated inside of `pages`
@@ -13,32 +13,16 @@ const TT_NEORIS = localFont({
 const SQUEEZY = localFont({ src: "../../public/fonts/SqueezyVF.woff2" });
 
 export default function Home() {
-  // ANIMATION SPEED (smaller = faster)
-  const speed = 7;
-
   const [currentLetter, setCurrentLetter] = useState("K");
   const [currentWidth, setCurrentWidth] = useState();
   const [snappedWidth, setSnappedWidth] = useState("400");
   const [renderedLetters, setRenderedLetters] = useState([currentLetter]);
   const [renderedLettersWidths, setRenderedLettersWidths] = useState([]);
-  const [positionValues, setpositionValues] = useState({});
 
-  const [positionX, setPositionX] = useState(400);
-  const [animationDirection, setAnimationDirection] = useState("forward");
-
-  const [count, setCount] = useState();
+  const [positionX, setPositionX] = useState(0);
 
   const themes = ["black", "red", "yellowGreen", "yellowBlue"];
-
   const [theme, setTheme] = useState(0);
-
-  const changeTheme = () => {
-    if (theme === themes.length - 1) {
-      setTheme(0);
-    } else {
-      setTheme(theme + 1);
-    }
-  };
 
   // const snappedWidths = [400, 430, 460, 490, 520, 550, 580, 610, 640, 670, 700];
   const snappedWidths = [400, 450, 500, 550, 600, 650, 700];
@@ -67,93 +51,51 @@ export default function Home() {
   const variableLinesSectionRef = useRef(null);
   const mainContainer = useRef();
 
-  const min = 0.6080125;
-  const max = 1.0880125;
+  const animeValue = {
+    posX: 400,
+  }
+  const themeValue = {
+    theme: 0
+  }
+
+  const animDuration = 1000
 
   useEffect(() => {
-    let center = window.innerWidth / 2;
-
-    let height = currentLetterRef.current.getBoundingClientRect().height;
-    const ratio = 300 / ((height * max - height * min) / 2);
-
-    const handleAnimation = (width) => {
-      // console.log(width);
-
-      const snappedWidth = snappedWidths.reduce(function (prev, curr) {
-        return Math.abs(curr - width) < Math.abs(prev - width) ? curr : prev;
-      });
-
-      setSnappedWidth(snappedWidth);
-      setCurrentWidth(width);
-
-      // console.log(width, snappedWidth, currentLetter);
-      // if (width >= 400 && width <= 700) {
-      //   setCurrentWidth(width);
-      // }
-    };
-
-    const handleResize = () => {
-      center = window.innerWidth / 2;
-      height = currentLetterRef.current.getBoundingClientRect().height;
-    };
-
-    // OLD APPROACH
-    // ------------
-
-    // setTimeout(() => {
-    //   if (positionX <= 400) {
-    //     setAnimationDirection("forward");
-    //   }
-
-    //   if (positionX >= 700) {
-    //     setAnimationDirection("backward");
-    //   }
-
-    //   if (animationDirection === "forward") {
-    //     setPositionX(positionX + 1);
-    //   } else if (animationDirection === "backward") {
-    //     setPositionX(positionX - 1);
-    //   }
-    //   handleAnimation(positionX);
-    //   // console.log(animationDirection, positionX);
-    // }, speed);
-
-    // NEW APPROACH
-    // ------------
-
-    const easeOutQuad = (t) => t * (2 - t);
-    const frameDuration = 1000 / 60;
-    const countTo = 300;
-    const duration = 5000;
-
-    let frame = 0;
-    const totalFrames = Math.round(duration / frameDuration);
-    const counter = setInterval(() => {
-      frame++;
-      const progress = easeOutQuad(frame / totalFrames);
-      // let progress = frame / totalFrames;
-
-      setCount(countTo * progress);
-      setPositionX(400 + count);
-      setPositionX(countTo * progress);
-      console.log(countTo * progress);
-      console.log(positionX);
-
-      if (frame === totalFrames) {
-        frame = 0;
+    // Anime.js animation to increment the value
+    anime({
+      targets: animeValue, // initial value
+      posX: 700, // final value
+      duration: animDuration, // animation duration in milliseconds
+      direction: 'alternate',
+      loop: true,
+      easing: 'easeInOutCubic',
+      round: 1, // round the value to the nearest integer
+      update: (anim) => {
+        setPositionX(animeValue.posX);
       }
-      handleAnimation(positionX);
-      console.log(positionX, count, progress);
-    }, frameDuration);
+    });
 
-    window.addEventListener("resize", handleResize);
-  });
-
-  // console.log(Math.floor(count));
+    anime({
+      targets: themeValue, // initial value
+      theme: themes.length, // final value
+      duration: animDuration * 8, // animation duration in milliseconds
+      loop: true,
+      easing: 'linear',
+      round: 1, // round the value to the nearest integer
+      update: (anim) => {
+        setTheme(themeValue.theme);
+      }
+    });
+  }, []);
 
   useEffect(() => {
-    handleAnimation(positionX);
-  }, [positionValues.positionX]);
+      const snappedWidth = snappedWidths.reduce(function (prev, curr) {
+        return Math.abs(curr - positionX) < Math.abs(prev - positionX) ? curr : prev;
+      });
+      setSnappedWidth(snappedWidth);
+      setCurrentWidth(positionX);
+
+  }, [positionX, snappedWidths]);
 
   // RANDOM LETTERS
   useEffect(() => {
@@ -170,15 +112,9 @@ export default function Home() {
   }, [snappedWidth]);
 
   // Update Theme on body
-
-  useEffect(() => {
-    changeTheme();
-    // console.log(theme);
-  }, [animationDirection]);
-
   useEffect(() => {
     document.body.className = themes[theme];
-  }, [theme]);
+  }, [theme, themes]);
 
   return (
     <main
